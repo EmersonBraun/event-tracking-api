ff01e372c4af116df8eed9589de555ee34ed688e
EmersonBraun <emersonfbraun@gmail.com>
2021-01-25 23:00:43 -0300

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { mainConfig } from './config/main.config';
 import { EventsModule } from './events/events.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(mainConfig),
-    EventsModule
+    EventsModule,
+    UserModule
   ],
 })
 export class AppModule {}
