dd8f4e3765587a62de661c45b808b0e64c6dad5c
EmersonBraun <emersonfbraun@gmail.com>
2021-01-25 23:01:45 -0300

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { mainConfig } from './config/main.config';
 import { EventsModule } from './events/events.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(mainConfig),
     EventsModule,
-    UserModule
+    UserModule,
+    AuthModule
   ],
 })
 export class AppModule {}
