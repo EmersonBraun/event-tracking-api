da25c5cba609ebb1b7ebc63e7ec3a93b2a33e48d
EmersonBraun <emersonfbraun@gmail.com>
2021-01-25 22:48:37 -0300

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { PrettyLogger } from './common/logger/pretty-log';
+import { setupDocumentation } from './config/documentation.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger: console });
+
+  app.use(helmet())
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 1000,
+    }),
+  )
+  app.useLogger(new PrettyLogger);
+  void setupDocumentation(app)
+  await app.listen(process.env.APP_PORT);
+  console.log(`🚀 ${String(process.env.APP_NAME)} (API) is running on: http://localhost:${process.env.APP_PORT}`);
 }
 bootstrap();
