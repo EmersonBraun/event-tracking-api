4c5fbfb915de6f0b8a7cef43f667d24f5f35bdd4
EmersonBraun <emersonfbraun@gmail.com>
2021-01-25 22:31:43 -0300

    chore: config default .env.example and lint-staged

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { mainConfig } from './config/main.config';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    ConfigModule.forRoot(mainConfig),
+    EventsModule
+  ],
 })
 export class AppModule {}
